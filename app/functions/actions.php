d48d72097d2066d62a1ff972030da6bbcf55d580
stacmv <stacmv@gmail.com>
2016-02-12 15:58:06 +0300

    * minor updates; + composer.lock

--- app/functions/actions.php
+++ app/functions/actions.php
@@ -4,15 +4,25 @@ function show_home_action(){
     global $_DATA;
     global $CFG;
     
-    set_topmenu_action();
+        
+    
+}
+
+function show_dashboard_action(){
+    global $_USER;
+    global $_DATA;
+    global $CFG;
+    
+    
     
     
 }
 function show_users_action(){
     global $_DATA;
-    
-    set_topmenu_action();
-    
+        
     $_DATA["users"] = get_users();
     
 }
+
+
+register_default_action("set_topmenu");
